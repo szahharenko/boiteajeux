7a0f1e40efe6aea94e4d412c0aa3de6b1dfe8be9
Sergei Zahharenko <acrashik@gmail.com>
2020-11-09 22:58:25 +0200

    Escaping name

--- boiteajeux.js
+++ boiteajeux.js
@@ -9,7 +9,7 @@ var createForm = function(game, code, name) {
  var cols = game.find('.clLigneBloc');
  if(cols.length == 5) {
   var id = cols[1].innerText;
-  cols[0].innerHTML = '<form id="form'+index+'" action="gestion.php" method="post" onsubmit="return validationUnique()"><input type="hidden" name="pAction" value="rejoindre"><input type="hidden" class="clInv" name="inv"><input type="hidden" name="id" value="'+code+'-'+id+'"><input type="image" src="./img/ico_join.png" alt="JOIN" class="clIcone" rel="popover" data-content="JOIN" onclick="return confirmJoin(' + index + ',\''+code+'\',\''+name+'\')" data-original-title=""></form>';
+  cols[0].innerHTML = '<form id="form'+index+'" action="gestion.php" method="post" onsubmit="return validationUnique()"><input type="hidden" name="pAction" value="rejoindre"><input type="hidden" class="clInv" name="inv"><input type="hidden" name="id" value="'+code+'-'+id+'"><input type="image" src="./img/ico_join.png" alt="JOIN" class="clIcone" rel="popover" data-content="JOIN" onclick="return confirmJoin(' + index + ',\''+code+'\',\''+escape(name)+'\')" data-original-title=""></form>';
  }
  var next = jQuery(game).next();
  if(next.length && !next.is('.clListeEntete')) createForm(next, code, name);
